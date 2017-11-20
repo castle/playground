57937c01156a4d452f894a2ab442df178f300e00
Johan Brissmyr <brissmyr@gmail.com>
2017-11-19 16:38:41 -0800

    Monkey patch for castle-rb 3.0.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 require 'castle/support/rails'
 
+module Castle
+  class HeaderFormatter
+    def call(header)
+      header.to_s.gsub(/^HTTP(?:_|-)/i, '').split(/_|-/).map(&:capitalize).join('-')
+    end
+  end
+end
+
 class SessionsController < Clearance::SessionsController
   before_filter :authorize, only: [:destroy]
 
